38dfba1dac60b61863ec42b6c75d72d288bbc0a2
ibaslogic <ibaskunle@gmail.com>
2023-01-06 16:28:44 +0100

    Fix GH-pages deployment issues with React Router

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './App.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
